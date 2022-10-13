629804bd5b126b55a1a4d17551ae07763dad554f
TheAtrAtr <dyadya_atr@mail.ru>
2022-10-13 22:16:23 +0300

    change name gamelist

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Engine {
-    private static final ArrayList<String> gamesList = new ArrayList<>();
+    private static final ArrayList<String> GAMES_LIST = new ArrayList<>();
 
     static {
-        gamesList.add("Exit");
-        gamesList.add("Greet");
-        gamesList.add("Even");
-        gamesList.add("Calc");
-        gamesList.add("GCD");
-        gamesList.add("Progression");
-        gamesList.add("Prime");
+        GAMES_LIST.add("Exit");
+        GAMES_LIST.add("Greet");
+        GAMES_LIST.add("Even");
+        GAMES_LIST.add("Calc");
+        GAMES_LIST.add("GCD");
+        GAMES_LIST.add("Progression");
+        GAMES_LIST.add("Prime");
     }
 
     public static void showMenu() {
         System.out.println("Please enter the game number and press Enter.");
-        for (int i = 1; i < gamesList.size(); i++) {
-            System.out.printf("%d - %s", i, gamesList.get(i));
+        for (int i = 1; i < GAMES_LIST.size(); i++) {
+            System.out.printf("%d - %s", i, GAMES_LIST.get(i));
             System.out.println();
         }
-        System.out.printf("%d - %s", 0, gamesList.get(0));
+        System.out.printf("%d - %s", 0, GAMES_LIST.get(0));
         System.out.println();
         System.out.print("Your choice: ");
         Scanner scanner = new Scanner(System.in);
